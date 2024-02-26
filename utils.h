be1ac796c33039b9e5156c6611876be2c3f6fc22
Luca Denti <ldenti@proton.me>
2024-02-26 22:44:32 +0100

    Move utils

--- utils.h
+++ utils.h
@@ -1,7 +1,43 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/resource.h>
 #include <sys/time.h>
+
+#include "kseq.h"
+
+static unsigned char seq_nt6_table[128] = {
+    0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1,
+    5, 2, 5, 5, 5, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 5, 5, 5,
+    5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 5, 2, 5, 5, 5, 3, 5, 5, 5, 5, 5, 5,
+    5, 5, 5, 5, 5, 5, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+
+static inline uint kputsn(const char *p, uint64_t l, kstring_t *s) {
+  if (s->l + l + 1 >= s->m) {
+    char *tmp;
+    s->m = s->l + l + 2;
+    kroundup32(s->m);
+    if ((tmp = (char *)realloc(s->s, s->m)))
+      s->s = tmp;
+    else
+      return EOF;
+  }
+  memcpy(s->s + s->l, p, l);
+  s->l += l;
+  s->s[s->l] = 0;
+  return l;
+}
+
+static inline double cputime() {
+  struct rusage r;
+  getrusage(RUSAGE_SELF, &r);
+  return (double)r.ru_utime.tv_sec + (double)r.ru_stime.tv_sec +
+         1e-6 * (double)(r.ru_utime.tv_usec + r.ru_stime.tv_usec);
+}
 
 static inline double realtime() {
   struct timeval tp;
