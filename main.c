010ba209d57ca6a9926bff5b505ddef773dcde4a
Luca Denti <ldenti@proton.me>
2024-03-11 09:51:02 +0100

    Remove record-by-record indexing

--- main.c
+++ main.c
@@ -1,15 +1,12 @@
 #include <string.h>
 
 int main_index(int argc, char *argv[]);
-int main_index_records(int argc, char *argv[]);
 int main_search(int argc, char *argv[]);
 int main_pingpong(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
   if (strcmp(argv[1], "index") == 0)
     return main_index(argc - 1, argv + 1);
-  else if (strcmp(argv[1], "index-rec") == 0)
-    return main_index_records(argc - 1, argv + 1);
   else if (strcmp(argv[1], "search") == 0)
     return main_search(argc - 1, argv + 1);
   else if (strcmp(argv[1], "pingpong") == 0)
