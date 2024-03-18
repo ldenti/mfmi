863a5fa8990849964d881a3cb9760746fd300a74
Luca Denti <ldenti@proton.me>
2024-03-18 11:07:48 +0100

    Index forward and reverse separately + Remove -r option (force FMD)

--- main_index.c
+++ main_index.c
@@ -9,21 +9,14 @@
 KSEQ_INIT(gzFile, gzread)
 
 int main_index(int argc, char *argv[]) {
-  (void)argc; // suppress unused parameter warning
-  double t_start = realtime();
-
   int nt = 1;
-  int reverse = 0;
-  uint32_t m = (uint32_t)(0.85 * UINT32_MAX) + 1;
+  // uint32_t m = (uint32_t)(INT32_MAX); // (uint32_t)(0.85 * UINT32_MAX) + 1;
 
   int c;
   while ((c = getopt(argc, argv, "@:rh")) >= 0) {
     switch (c) {
     case '@':
       nt = atoi(optarg);
-      continue;
-    case 'r':
-      reverse = 1;
       continue;
     case 'h':
       printf("HELP\n");
@@ -38,64 +31,75 @@ int main_index(int argc, char *argv[]) {
     return 1;
   }
 
-  int i, l;
-  char *fa_path;
   rlcsa_t *rlc = rlc_init();
+
   gzFile fp;
   kseq_t *ks;
   uint8_t *s;
   kstring_t buf = {0, 0, 0};
+  kstring_t buf_rc = {0, 0, 0};
+  int i, l;
+  char *fa_path;
+  double ct, rt;
   while (optind < argc) {
     fa_path = argv[optind++];
-
     fp = gzopen(fa_path, "rb");
     ks = kseq_init(fp);
     while ((l = kseq_read(ks)) >= 0) {
-      s = (uint8_t *)ks->seq.s;
-
-      // change encoding
-      for (i = 0; i < l; ++i)
-        s[i] = fm6(s[i]);
-
-      // Add forward to buffer
-      kputsn((char *)s, l + 1, &buf);
-      if (reverse) {
-        // Add reverse to buffer
-        for (i = 0; i < (l >> 1); ++i) {
-          int tmp = s[l - 1 - i];
-          tmp = fm6_comp(tmp);
-          s[l - 1 - i] = fm6_comp(s[i]);
-          s[i] = tmp;
-        }
-        if (l & 1)
-          s[i] = fm6_comp(s[i]);
-        kputsn((char *)s, l + 1, &buf);
-      }
-      if (buf.l >= m) {
-        double ct = cputime(), rt = realtime();
+      if (buf.l + l > INT32_MAX) {
+        ct = cputime(), rt = realtime();
         rlc_insert(rlc, (const uint8_t *)buf.s, (uint32_t)buf.l, nt);
         fprintf(stderr,
                 "[M::%s] inserted %ld symbols in %.3f sec; %.3f CPU sec\n",
                 __func__, (long)buf.l, realtime() - rt, cputime() - ct);
         buf.l = 0;
+        ct = cputime(), rt = realtime();
+        rlc_insert(rlc, (const uint8_t *)buf_rc.s, (uint32_t)buf_rc.l, nt);
+        fprintf(stderr,
+                "[M::%s] inserted %ld symbols in %.3f sec; %.3f CPU sec\n",
+                __func__, (long)buf_rc.l, realtime() - rt, cputime() - ct);
+        buf_rc.l = 0;
       }
+
+      s = (uint8_t *)ks->seq.s;
+
+      // change encoding
+      for (i = 0; i < l; ++i)
+        s[i] = s[i] < 128 ? seq_nt6_table[s[i]] : 5;
+
+      // Add forward to buffer
+      kputsn((char *)s, l + 1, &buf);
+
+      // Add reverse to buffer
+      for (i = 0; i < (l >> 1); ++i) {
+        int tmp = s[l - 1 - i];
+        tmp = (tmp >= 1 && tmp <= 4) ? 5 - tmp : tmp;
+        s[l - 1 - i] = (s[i] >= 1 && s[i] <= 4) ? 5 - s[i] : s[i];
+        s[i] = tmp;
+      }
+      if (l & 1)
+        s[i] = (s[i] >= 1 && s[i] <= 4) ? 5 - s[i] : s[i];
+      kputsn((char *)s, ks->seq.l + 1, &buf_rc);
     }
     if (buf.l) {
-      double ct = cputime(), rt = realtime();
+      ct = cputime(), rt = realtime();
       rlc_insert(rlc, (const uint8_t *)buf.s, (uint32_t)buf.l, nt);
       fprintf(stderr,
               "[M::%s] inserted %ld symbols in %.3f sec; %.3f CPU sec\n",
               __func__, (long)buf.l, realtime() - rt, cputime() - ct);
       buf.l = 0;
+      ct = cputime(), rt = realtime();
+      rlc_insert(rlc, (const uint8_t *)buf_rc.s, (uint32_t)buf_rc.l, nt);
+      fprintf(stderr,
+              "[M::%s] inserted %ld symbols in %.3f sec; %.3f CPU sec\n",
+              __func__, (long)buf_rc.l, realtime() - rt, cputime() - ct);
+      buf_rc.l = 0;
     }
     kseq_destroy(ks);
     gzclose(fp);
-
-    fprintf(stderr,
-            "[M::%s] indexed %s - Total time: %.3f sec; CPU: %.3f sec\n",
-            __func__, fa_path, realtime() - t_start, cputime());
   }
   free(buf.s);
+  free(buf_rc.s);
 
   // Print BWT - debug
   // rpitr_t *it = calloc(1, sizeof(rpitr_t));
@@ -103,13 +107,11 @@ int main_index(int argc, char *argv[]) {
   // uint8_t *b;
   // while ((b = (uint8_t *)rope_itr_next_block(it)) != 0)
   //   rle_print(b, 1);
-
+  ct = cputime(), rt = realtime();
   rlc_dump(rlc, "-"); // TODO: add path to CLI
   rlc_destroy(rlc);
-
-  fprintf(stderr,
-          "[M::%s] dumped index - Total time: %.3f sec; CPU: %.3f sec\n",
-          __func__, realtime() - t_start, cputime());
+  fprintf(stderr, "[M::%s] dumped index in  %.3f sec; %.3f CPU sec\n", __func__,
+          realtime() - rt, cputime() - ct);
 
   return 0;
 }
