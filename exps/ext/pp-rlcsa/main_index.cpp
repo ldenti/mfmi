80dde93759a0531fd05f91e3cd514fe3fa678891
Luca Denti <ldenti@proton.me>
2024-05-07 15:59:01 +0200

    Update implementation based on rlcsa

--- exps/ext/pp-rlcsa/main_index.cpp
+++ exps/ext/pp-rlcsa/main_index.cpp
@@ -1,17 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdint>
+#include <getopt.h>
+#include <iostream>
 #include <string>
+#include <vector>
 #include <zlib.h>
 
 #include "kseq.h"
 #include "rlcsa.h"
 #include "rlcsa_builder.h"
 
-#include "utils.h"
-
 KSEQ_INIT(gzFile, gzread)
 
-int main_index(int argc, char *argv[]) {
+static const unsigned char rc[128] = {
+    'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N',
+    'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N',
+    'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N',
+    'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N',
+    'N', 'N', 'N', 'N', 'N', 'T', 'N', 'G', 'N', 'N', 'N', 'C', 'N', 'N', 'N',
+    'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'A', 'N', 'N', 'N', 'N', 'N',
+    'N', 'N', 'N', 'N', 'N', 'N', 'N', 'T', 'N', 'G', 'N', 'N', 'N', 'C', 'N',
+    'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'A', 'N', 'N', 'N',
+    'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N'};
+
+uint64_t get_size_fa(const char *fa_path) {
+  gzFile fp = gzopen(fa_path, "r");
+  kseq_t *seq = kseq_init(fp);
+  uint64_t tot_l = 0;
+  int l;
+  while ((l = kseq_read(seq)) >= 0)
+    tot_l += l + 1;
+  kseq_destroy(seq);
+  gzclose(fp);
+  return tot_l;
+}
+
+void concatenate_fa(const char *fa_path, uint64_t size, unsigned char *data,
+                    unsigned char *data_r) {
+  gzFile fp = gzopen(fa_path, "r");
+  kseq_t *ks = kseq_init(fp);
+  int l, i;
+  uint64_t curr_l = 0;
+  while ((l = kseq_read(ks)) >= 0) {
+    memmove(data + curr_l, ks->seq.s, l);
+
+    // reverse
+    for (i = 0; i < (l >> 1); ++i) {
+      int tmp = ks->seq.s[l - 1 - i];
+      tmp = rc[tmp];
+      ks->seq.s[l - 1 - i] = rc[ks->seq.s[i]];
+      ks->seq.s[i] = tmp;
+    }
+    if (l & 1)
+      ks->seq.s[i] = rc[ks->seq.s[i]];
+    memmove(data_r + curr_l, ks->seq.s, l);
+
+    curr_l += l + 1;
+  }
+  kseq_destroy(ks);
+  gzclose(fp);
+}
+
+int main_index(int argc, char **argv) {
   uint sample_rate = 0; // (1 << 31);
   int block_size = 32;
   int threads = 1;
@@ -33,103 +82,57 @@ int main_index(int argc, char *argv[]) {
   if (argc - optind < 1) {
     return 1;
   }
+  std::vector<std::string> inputs;
+  while (optind < argc)
+    inputs.push_back(argv[optind++]);
 
+  std::cerr << "Initializing.." << std::endl;
   CSA::RLCSABuilder builder(block_size, sample_rate, 0, threads, NULL);
 
-  gzFile fp;
-  kseq_t *ks;
-  uint8_t *s;
-  kstring_t buf = {0, 0, 0};
-  kstring_t buf_rc = {0, 0, 0};
-  int i, l;
-  char *fa_path;
-  CSA::RLCSA *index;
-  double rt;
-  while (optind < argc) {
-    fa_path = argv[optind++];
-    fp = gzopen(fa_path, "rb");
-    ks = kseq_init(fp);
-    while ((l = kseq_read(ks)) >= 0) {
-      if (buf.l + l > INT32_MAX) {
-        rt = realtime();
-        index = new CSA::RLCSA((CSA::uchar *)buf.s, buf.l, block_size,
-                               sample_rate, threads, false);
-        index->writeTo(fa_path); // TODO: store to tmp dir
-        delete index;
-        builder.insertFromFile(fa_path, (CSA::uchar *)buf.s);
-        fprintf(stderr, "[M::%s] inserted %ld symbols in %.3f sec\n", __func__,
-                (long)buf.l, realtime() - rt);
-        buf.l = 0;
+  uint64_t max_size = ((uint64_t)1 << 32) - 1;
+  unsigned char *data =
+      (unsigned char *)calloc(max_size, sizeof(unsigned char));
+  unsigned char *data_r =
+      (unsigned char *)calloc(max_size, sizeof(unsigned char));
+  for (const std::string &fa_path : inputs) {
+    std::cerr << "Reading " << fa_path << std::endl;
+    uint64_t size = get_size_fa(fa_path.c_str());
+    concatenate_fa(fa_path.c_str(), size, data, data_r);
 
-        rt = realtime();
-        index = new CSA::RLCSA((CSA::uchar *)buf_rc.s, buf_rc.l, block_size,
-                               sample_rate, threads, false);
-        index->writeTo(fa_path); // TODO: store to tmp dir
-        delete index;
-        builder.insertFromFile(fa_path, (CSA::uchar *)buf_rc.s);
-        fprintf(stderr, "[M::%s] inserted %ld symbols in %.3f sec\n", __func__,
-                (long)buf_rc.l, realtime() - rt);
-        buf_rc.l = 0;
-      }
+    std::cerr << "Indexing " << fa_path << std::endl;
+    CSA::RLCSA *index =
+        new CSA::RLCSA(data, size, block_size, sample_rate, threads, false);
+    std::cerr << "Storing " << fa_path << std::endl;
+    if (index != 0 && index->isOk())
+      index->writeTo(fa_path);
+    delete index;
+    std::cerr << "Merging " << fa_path << std::endl;
+    builder.insertFromFile(fa_path, data);
 
-      s = (uint8_t *)ks->seq.s;
-
-      // change encoding
-      for (i = 0; i < l; ++i)
-        s[i] = s[i] < 128 ? seq_nt6_table[s[i]] : 5;
-
-      // Add forward to buffer
-      kputsn((char *)s, l + 1, &buf);
-
-      // Add reverse to buffer
-      for (i = 0; i < (l >> 1); ++i) {
-        int tmp = s[l - 1 - i];
-        tmp = (tmp >= 1 && tmp <= 4) ? 5 - tmp : tmp;
-        s[l - 1 - i] = (s[i] >= 1 && s[i] <= 4) ? 5 - s[i] : s[i];
-        s[i] = tmp;
-      }
-      if (l & 1)
-        s[i] = (s[i] >= 1 && s[i] <= 4) ? 5 - s[i] : s[i];
-      kputsn((char *)s, ks->seq.l + 1, &buf_rc);
-    }
-    if (buf.l) {
-      rt = realtime();
-      index = new CSA::RLCSA((CSA::uchar *)buf.s, buf.l, block_size,
-                             sample_rate, threads, false);
-      index->writeTo(fa_path); // TODO: store to tmp dir
-      delete index;
-      builder.insertFromFile(fa_path, (CSA::uchar *)buf.s);
-      fprintf(stderr, "[M::%s] inserted %ld symbols in %.3f sec\n", __func__,
-              (long)buf.l, realtime() - rt);
-      buf.l = 0;
-
-      rt = realtime();
-      index = new CSA::RLCSA((CSA::uchar *)buf_rc.s, buf_rc.l, block_size,
-                             sample_rate, threads, false);
-      index->writeTo(fa_path); // TODO: store to tmp dir
-      delete index;
-      builder.insertFromFile(fa_path, (CSA::uchar *)buf_rc.s);
-      fprintf(stderr, "[M::%s] inserted %ld symbols in %.3f sec\n", __func__,
-              (long)buf_rc.l, realtime() - rt);
-      buf_rc.l = 0;
-    }
-    kseq_destroy(ks);
-    gzclose(fp);
+    std::cerr << "Indexing " << fa_path << " (R)" << std::endl;
+    index =
+        new CSA::RLCSA(data_r, size, block_size, sample_rate, threads, false);
+    std::cerr << "Storing " << fa_path << " (R)" << std::endl;
+    char *rev = (char *)malloc(fa_path.size() + 2);
+    strcpy(rev, fa_path.c_str());
+    strcat(rev, ".R");
+    if (index != 0 && index->isOk())
+      index->writeTo(rev);
+    delete index;
+    std::cerr << "Merging " << fa_path << " (R)" << std::endl;
+    builder.insertFromFile(rev, data_r);
   }
-  free(buf.s);
-  free(buf_rc.s);
+  free(data);
 
-  rt = realtime();
-  index = builder.getRLCSA();
-  if (index != 0 && index->isOk()) {
-    // std::cout << std::endl;
-    // rlcsa->printInfo();
-    // rlcsa->reportSize(true);
-    index->writeTo(index_prefix);
+  std::cerr << "Storing full index to " << index_prefix << std::endl;
+  CSA::RLCSA *rlcsa = builder.getRLCSA();
+  if (rlcsa != 0 && rlcsa->isOk()) {
+    std::cout << std::endl;
+    rlcsa->printInfo();
+    rlcsa->reportSize(true);
+    rlcsa->writeTo(index_prefix);
   }
-  fprintf(stderr, "[M::%s] dumped index in %.3f sec\n", __func__,
-          realtime() - rt);
-  delete index;
+  delete rlcsa;
 
   return 0;
 }
--- exps/ext/pp-rlcsa/main_pp.cpp
+++ exps/ext/pp-rlcsa/main_pp.cpp
@@ -3,22 +3,16 @@
 
 #include "kseq.h"
 
-#include "fmd_simple.hpp"
+#include "fmd.h"
 
 KSEQ_INIT(gzFile, gzread)
 
-std::string interval2str(const FMDPosition &i) {
-  return std::to_string(i.forward_start) + "," +
-         std::to_string(i.reverse_start) + "," +
-         std::to_string(i.end_offset + 1);
-}
-
-void pp(const FMD *index, uint8_t *seq, const int64_t l, const char *qname) {
+void pp(const CSA::FMD *index, char *seq, const int64_t l, const char *qname) {
   bool first = true;
 
   int begin = l - 1;
   uint8_t c;
-  FMDPosition sai;
+  CSA::FMDPosition sai;
   while (begin >= 0) {
     c = seq[begin];
     sai = index->getCharPosition(c);
@@ -35,6 +29,7 @@ void pp(const FMD *index, uint8_t *seq, const int64_t l, const char *qname) {
 
     // Forward search
     int end = begin;
+    // std::cerr << "F from " << end << std::endl;
     c = seq[end];
     sai = index->getCharPosition(c);
     while (!sai.isEmpty()) {
@@ -43,9 +38,9 @@ void pp(const FMD *index, uint8_t *seq, const int64_t l, const char *qname) {
       sai = index->extend(sai, c, false);
     }
 
-    // add solution
+    // print solution
     std::cout << (first ? qname : "*") << "\t" << begin << "\t"
-              << end - begin + 1 << std::endl;
+              << end << "\t" << end - begin + 1 << std::endl;
     first = false;
 
     // prepare for next round
@@ -61,23 +56,17 @@ int main_pingpong(int argc, char *argv[]) {
   char *index_prefix = argv[1];
   char *fq_fn = argv[2];
 
-  const FMD *index = new FMD(index_prefix, false);
+  const CSA::FMD *index = new CSA::FMD(index_prefix, false);
 
   gzFile fp = gzopen(fq_fn, "rb");
   kseq_t *ks = kseq_init(fp);
-  int l, i;
-  uint8_t *s;
+  int l;
   while ((l = kseq_read(ks)) >= 0) {
-    s = (uint8_t *)ks->seq.s;
-
-    // change encoding
-    for (i = 0; i < l; ++i)
-      s[i] = s[i] < 128 ? seq_nt6_table[s[i]] : 5;
-
-    pp(index, s, l, ks->name.s);
+    pp(index, ks->seq.s, l, ks->name.s);
   }
   kseq_destroy(ks);
   gzclose(fp);
+
   delete index;
 
   return 0;
